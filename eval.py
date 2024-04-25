3e05f893fae0196455134ab5aa07f4f043039cd4
Sam Sampson <innovatests@gmail.com>
2024-04-24 17:59:03 -0700

    Implement precision at k

--- eval.py
+++ eval.py
@@ -74,3 +74,40 @@ def hit_rate(model,
 
         hit_rate = hits / num_users
         return (hit_rate, users_to_k_ratings)
+
+
+
+def precision_at_k(model,
+                   k,
+                   num_users,
+                   num_movies,
+                   data,
+                   device,
+                   batch_size=100):
+    with torch.no_grad():
+        users_to_k_ratings = {}
+        hits = 0
+
+        movie_batches = split_given_size(np.arange(num_movies), batch_size)
+
+        for user in tqdm(range(num_users)):
+            mheap = MovieRatingsHeap(k)
+            for movie_batch in movie_batches:
+                user_batch = np.ones_like(movie_batch) * user
+                user_tensor = torch.tensor(user_batch).to(device)
+                movie_tensor = torch.tensor(movie_batch).to(device)
+
+                predicted_rating_tensor = model.forward(user_tensor, movie_tensor)
+                pred = predicted_rating_tensor.cpu().numpy()
+                for i in range(len(movie_batch)):
+                    movie = movie_batch[i]
+                    p = pred[i]
+                    mheap.add_rating(p, movie)
+
+            users_to_k_ratings[user] = mheap.get_top_ratings()
+            for rating_unused, movie in mheap.get_top_ratings():
+                if ((data['user_id'] == user) & (data['movie_id'] == movie)).any():
+                    hits += 1
+
+        hit_rate = hits / (num_users * k)
+        return (hit_rate, users_to_k_ratings)
